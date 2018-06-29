9f19d9b8f4b2a47d392c63b66f76e48af39bf14e
aviral srivastava <aviral.srivastava@wingify.com>
2018-06-29 18:10:08 +0530

    adds newline in raw inputting of hashtags.

--- set_hashtag.py
+++ set_hashtag.py
@@ -1,4 +1,4 @@
-hashtag = raw_input("Please enter the hashtag for which you want to stream the match.")
+hashtag = raw_input("Please enter the hashtag for which you want to stream the match.\n")
 text_file = open("hashtag.txt", "w")
 text_file.write(hashtag)
 text_file.close()
