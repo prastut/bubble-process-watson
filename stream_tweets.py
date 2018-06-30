074f6397e439968c3643370efb686d07620866c3
aviral srivastava <aviral.srivastava@wingify.com>
2018-06-30 18:39:29 +0530

    adds the module for streaming tweets.

--- stream_tweets.py
+++ stream_tweets.py
@@ -13,10 +13,8 @@ import tweepy
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 producer = KafkaProducer(retries=5)
-text_file = open("hashtag.txt", "r")
-hashtag = text_file.readline(hashtag)
-text_file.close()
-topic = hashtag + str(datetime.datetime.utcnow().date())
+
+topic = argentina-iceland-june-16
 
 
 class StreamListener(tweepy.StreamListener):
@@ -26,19 +24,18 @@ class StreamListener(tweepy.StreamListener):
     idSelf = 0
 
     def on_status(self, status):
+        print("On Status")
         if status.retweeted:
             return
-        id = status.id
         try:
             self.tweet["userProfile"] = status.user._json[
                 "profile_image_url_https"]
             self.idSelf += 1
             self.tweet["tweet"] = status.text.encode('utf8')
-            self.tweet["id"] = id
+            self.tweet["id"] = status.id
             self.tweet["sequence"] = self.idSelf
             self.tweet["created_at"] = status.created_at
-            future = producer.send(
-                topic, bytes(self.tweet))
+            future = producer.send(topic, bytes(self.tweet))
         except Exception as e:
             print e
         finally:
@@ -50,9 +47,9 @@ class StreamListener(tweepy.StreamListener):
             return False
 
 auth = tweepy.OAuthHandler(
-    settingsTwitter.TWITTER_APP_KEY, settingsTwitter.TWITTER_APP_SECRET)
-auth.set_access_token(settingsTwitter.TWITTER_KEY,
-                      settingsTwitter.TWITTER_SECRET)
+    settings_twitter.TWITTER_APP_KEY, settings_twitter.TWITTER_APP_SECRET)
+auth.set_access_token(settings_twitter.TWITTER_KEY,
+                      settings_twitter.TWITTER_SECRET)
 api = tweepy.API(auth)
 print "Twitter API Authentication is successful!"
 stream_listener = StreamListener()
@@ -61,12 +58,12 @@ stream_listener = StreamListener()
 def startStream():
     print "start stream"
     while True:
-      try:
-        print "trying"
-        stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-        print "Connection made."
-        stream.filter(languages=["en"], track=[str(hashtag)])
-      except Exception as e:
-        print(e)
+        try:
+            print "trying"
+            stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+            print "Connection made."
+            stream.filter(languages=["en"], track=["Mandsaur"])
+        except Exception as e:
+            print(e)
 
 startStream()
